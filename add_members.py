dfe76c4922407fe244e718f3c9fb2f93a22e2acc
craighillelson <craighillelson@gmail.com>
2020-08-13 07:48:30 -0400

    update add_members.py, functions.py
    simplified import statement
    removed unnecessary parameter from get_domain function

--- add_members.py
+++ add_members.py
@@ -3,11 +3,16 @@ Open a csv and populate a dictionary with its contents. Prompt the user to
 add members.
 """
 
-import functions
+from functions import (open_csv_populate_lst,
+                       get_domain,
+                       prompt_user_for_domain,
+                       prompt_user_for_prefix,
+                       concat_lists,
+                       write_lst_to_csv)
 
-employees = functions.open_csv_populate_lst('employees.csv')
-domain = functions.get_domain('email', employees)
-domain_confirmed = functions.prompt_user_for_domain(domain)
-employees_to_add = functions.prompt_user_for_prefix(domain, employees)
-all_employees = functions.concat_lists(employees, employees_to_add)
-functions.write_lst_to_csv('employees.csv', all_employees, ['email'])
+employees = open_csv_populate_lst('employees.csv')
+domain = get_domain(employees)
+domain_confirmed = prompt_user_for_domain(domain)
+employees_to_add = prompt_user_for_prefix(domain, employees)
+all_employees = concat_lists(employees, employees_to_add)
+write_lst_to_csv('employees.csv', all_employees, ['email'])
--- functions.py
+++ functions.py
@@ -21,7 +21,7 @@ def open_csv_populate_lst(file_name):
     return lst
 
 
-def get_domain(lst1, lst2):
+def get_domain(lst2):
     """Extract the domain from an email address."""
     lst1 = lst2[0]
     return lst1.split('@')[1]
