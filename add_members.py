0322d6031623573c004a3d9bffb2d5740cff02a2
Craig Hillelson <craighillelson@gmail.com>
2020-10-07 22:55:23 +0000

    update add_members.py
    changed string quoting to be consistent with other repos.

--- add_members.py
+++ add_members.py
@@ -10,9 +10,9 @@ from functions import (open_csv_populate_lst,
                        concat_lists,
                        write_lst_to_csv)
 
-employees = open_csv_populate_lst('employees.csv')
+employees = open_csv_populate_lst("employees.csv")
 domain = get_domain(employees)
 domain_confirmed = prompt_user_for_domain(domain)
 employees_to_add = prompt_user_for_prefix(domain, employees)
 all_employees = concat_lists(employees, employees_to_add)
-write_lst_to_csv('employees.csv', all_employees, ['email'])
+write_lst_to_csv("employees.csv", all_employees, ["email"])
